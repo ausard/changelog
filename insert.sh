ef9d7af710745eedfe2f9fdba03fe6ca24a05187
Aliaksei_Sabetski <AS828423@bose.com>
2021-02-24 15:20:06 +0300

    MM

--- insert.sh
+++ insert.sh
@@ -4,4 +4,4 @@ match_end='## 1.1.0 (2021-02-24)'
 insert='[NEXT_RELEASE_VERSION]'
 changelog_file="CHANGELOG.md"
 # sed -i '/\[$match_end\]/i $insert' $changelog_file
-sed -i '6 a ## [NEXT_RELEASE_VERSION]' $changelog_file
+sed -i '6 a ## [NEXT_RELEASE_VERSION]\n### Added\\Updated\\Delete \n- \n' $changelog_file
