4cc5b09f5226ee8431ad705c0b3239c20174455f
Aliaksei_Sabetski <AS828423@bose.com>
2021-02-25 12:30:03 +0300

    ters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added|Updated|Delete 
 - 
 
+## 1.3.0
+### Added|Updated|Delete 
+- 
--- insert.sh
+++ insert.sh
@@ -1,14 +1,22 @@
+echo "Insert pattern in changelog"
 latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+
 changelog_file="CHANGELOG.md"
-  if [ -f "$changelog_file" ]; then
-    echo "Insert pattern in changelog"
-    number_line=$(grep -n $latest_tag $changelog_file | cut -d : -f 1)
-    if [ $number_line -gt 0 ]; then
-        sed -i $number_line' i ## [NEXT_RELEASE_VERSION]\n### Added\|Updated\|Delete \n- \n' $changelog_file
-    else
-        sed -i '$ a ## [NEXT_RELEASE_VERSION]\n### Added\|Updated\|Delete \n- \n' $changelog_file
-    fi 
-    git add $changelog_file
-    git commit -m "update_changelog_skip_build"
-    git push 
-  fi
+if [ ! -f "$changelog_file" ]; then  
+    echo '# Changelog
+All notable changes to this project will be documented in this file.
+ 
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+' > CHANGELOG.md 
+fi
+number_line=$(grep -n '## '$latest_tag'' $changelog_file | cut -d : -f 1)
+if [ $number_line -gt 0 ]; then    
+    sed -i $number_line' i ## [NEXT_RELEASE_VERSION]\n### Added\|Updated\|Delete \n- \n' $changelog_file
+else
+    number_line2=$(grep -n 'and this project adheres to \[Semantic Versioning\]' $changelog_file | cut -d : -f 1)
+    sed -i $number_line2' a \\n## [NEXT_RELEASE_VERSION]\n### Added\|Updated\|Delete \n- ' $changelog_file
+fi 
+# git add $changelog_file
+# git commit -m "update_changelog_skip_build"
+# git push 
